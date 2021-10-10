f77cb687049e595dc9079bb5aa37eeedc005a8fc
Juan Litvin <litvinjuan@gmail.com>
2021-10-10 12:25:33 -0300

    Add inertia client & install ziggy vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+import { ZiggyVue } from 'ziggy';
+import { Ziggy } from './ziggy';
+
+InertiaProgress.init()
+
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el)
+    },
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +12,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require("tailwindcss"),
-    ]);
+mix.js('resources/js/app.js', 'public/js').vue({ version: 3 })
+
+mix.postCss('resources/css/app.css', 'public/css', [
+    require("tailwindcss"),
+])
+
+mix.alias({
+    ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue'),
+});
